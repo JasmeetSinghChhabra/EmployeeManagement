1f64e03890eff34a3bcdc54380d8970e68b3cb18
Jasmeet Singh Chhabra <jasmeet.chhabra@accuv.com>
2019-09-28 02:12:43 -0400

    Added Routes with Api Data

---
+++ src/components/AboutPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class AboutPage extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <h2> About </h2>
+        <p>This is About page</p>
+      </React.Fragment>
+    );
+  }
+}
+
+export default AboutPage;
---
+++ src/components/HomePage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function HomePage() {
+  return (
+    <div className="jumbotron">
+      <h1> PLuralsight Administration</h1>
+      <p>React is so good</p>
+      <a href="/about">About</a>
+    </div>
+  );
+}
+
+export default HomePage;
